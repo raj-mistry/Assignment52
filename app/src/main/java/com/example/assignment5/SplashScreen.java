f3433146d3b2df36ac85166c42b059e638169585
raj-mistry <56315897+raj-mistry@users.noreply.github.com>
2020-10-12 11:52:34 -0400

    8

--- app/src/main/java/com/example/assignment5/SplashScreen.java
+++ app/src/main/java/com/example/assignment5/SplashScreen.java
@@ -1,6 +1,7 @@
 package com.example.assignment5;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,19 +9,24 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class SplashScreen extends AppCompatActivity {
+    MediaPlayer myMusic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        myMusic = MediaPlayer.create(this,R.raw.song);
+        myMusic.start();
         TimerTask task = new TimerTask(){
             @Override
             public void run(){
+                myMusic.pause();
                 finish();
                 startActivity(new Intent(SplashScreen.this,ItemListActivity.class));
             }
         };
         Timer opening = new Timer();
         opening.schedule(task,5000);
+
     }
 }
